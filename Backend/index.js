655d83eb32e8ce8e42c3fc46d97e39daf0983476
Nexisparkx <nexisparkx@gmail.com>
2024-11-19 02:43:34 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('short'));
 app.use(cors({
-    origin: ['http://192.168.34.106:3000', 'http://localhost:3000','http://localhost:5173'],
+    origin: ['http://192.168.34.106:3000', 'http://localhost:3000','http://localhost:5173',https://nexisparkx.vercel.app],
     credentials: true,
 }));
 
